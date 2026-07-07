770618fb8a18181019db0baf1aa2edf819dc5841
CricetusHamster <sillybubbybaba.2001@gmail.com>
2026-07-07 16:58:09 +0800

    Activity 1

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("   ╔═══════════╗");
+        System.out.println("   ║ PWU JASMS ║");
+        System.out.println("   ╚═══════════╝");
+        System.out.println("");
+        System.out.println("Welcome! :)");
+        System.out.println("");
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=");
+        System.out.println(" Pixels to Possibilities.");
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=");
+        System.out.println("");
+        System.out.println("Teacher: Sir Lan");
+}
+}
