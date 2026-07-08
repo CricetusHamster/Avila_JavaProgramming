78a7b9b9747df0b1ea975e5ed4619af7d53a6d10
CricetusHamster <sillybubbybaba.2001@gmail.com>
2026-07-08 16:53:16 +0800

    Performance Task 1

--- Main.java
+++ Main.java
@@ -1,17 +1,52 @@
+// Performance Task 1
+/*
+//  Java is sooo complicated haha :P
+*/
 public class Main {
-
     public static void main(String[] args) {
+        // School Logo
         System.out.println("");
-        System.out.println("   ╔═══════════╗");
-        System.out.println("   ║ PWU JASMS ║");
-        System.out.println("   ╚═══════════╝");
+        System.out.println("▐▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▌\r\n" + //
+                           "▐   _______          ___    _        _          _____ __  __  _____   ▌\r\n" + //
+                           "▐  |  __ \\ \\        / / |  | |      | |  /\\    / ____|  \\/  |/ ____|  ▌\r\n" + //
+                           "▐  | |__) \\ \\  /\\  / /| |  | |      | | /  \\  | (___ | \\  / | (___    ▌\r\n" + //
+                           "▐  |  ___/ \\ \\/  \\/ / | |  | |  _   | |/ /\\ \\  \\___ \\| |\\/| |\\___ \\   ▌\r\n" + //
+                           "▐  | |      \\  /\\  /  | |__| | | |__| / ____ \\ ____) | |  | |____) |  ▌\r\n" + //
+                           "▐  |_|       \\/  \\/    \\____/   \\____/_/    \\_\\_____/|_|  |_|_____/   ▌\r\n" + //
+                           "▐▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▌");
+        // It looks uneven but it will look properly once executed
         System.out.println("");
+        System.out.println("");
+        // Intro
         System.out.println("Welcome! :)");
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        System.out.println("             Pixels Into Possibilities");
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        System.out.println("Teacher: Sir Lan Sevilleno       Created By: Emman");
+        System.out.println("Quote of the Day: The chances are low but never zero");
         System.out.println("");
-        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=");
-        System.out.println(" Pixels to Possibilities.");
-        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=");
+        // Name and Section
+        System.out.println("~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^");
+        System.out.print("Hi! ");
+        System.out.print("I'm ");
+        System.out.println("Reinell Emmanuel B. Avila");
+        System.out.print("From ");
+        System.out.println("Grade 12 - Aquinas: ICT");
+        System.out.println("~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^");
         System.out.println("");
-        System.out.println("Teacher: Sir Lan");
-}
+        System.out.println("In my understanding, ICT is about everything tech-related, which makes it broad.");
+        System.out.println("I chose ICT because I've loved tech ever since I was a kid.");
+        System.out.println("In the future, I want to be an IT professional.");
+        System.out.println("Never Give Up!");
+        System.out.println("");
+        System.out.println("       _.---._    /\\\\\r\n" + //
+                           "    ./'       \"--`\\//\r\n" + //
+                           "  ./              o \\\r\n" + //
+                           " /./\\  )______   \\__ \\\r\n" + //
+                           "./  / /\\ \\   | \\ \\  \\ \\\r\n" + //
+                           "   / /  \\ \\  | |\\ \\  \\7\r\n" + //
+                           "    \"     \"    \"  \"             - An Aardvark");
+        // I know this looks weird in code but this looks very well once executed
+        System.out.println("");
+    }
 }
