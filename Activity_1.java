7a4f4af1aba984c6d510046ac7dc4ff84d9da899
CricetusHamster <sillybubbybaba.2001@gmail.com>
2026-07-10 21:32:22 +0800

    Activity 1

---
+++ Activity_1.java
@@ -0,0 +1,32 @@
+public class Activity_1 {
+    public static void main(String[] args) {
+        // Variables and Data
+        String firstName = "Reinell Emmanuel ";
+        char middleName = 'B';
+        String lastName = "Avila";
+        String fullName = firstName + middleName + ". " + lastName;
+        String nickname = "Emman";
+        int age = 20;
+        int gradeLevel = 12;
+        String section = "Aquinas";
+        String school = "PWU JASMS QC";
+        String favoriteSubject = "Computer Programming";
+        float averageGrade = 85.67f;
+        boolean honorStudent = false;
+        // Display
+        System.out.println("================================================");
+        System.out.println("              STUDENT INFORMATION");
+        System.out.println("================================================");
+        System.out.println();
+        System.out.println("Name             : " + fullName);
+        System.out.println("Age              : " + age);
+        System.out.println("Grade Level      : " + gradeLevel);
+        System.out.println("Section          : " + section);
+        System.out.println("School           : " + school);
+        System.out.println("Favorite Subject : " + favoriteSubject);
+        System.out.println("Average Grade    : " + averageGrade);
+        System.out.println("Honor Student    : " + honorStudent);
+        System.out.println();
+        System.out.println("================================================");
+    }
+}
